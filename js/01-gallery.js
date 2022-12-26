d0659a75071e5201eec28a20ad856795736ad138
Maryna <yasinskamarina@gmail.com>
2022-12-26 21:43:09 +0200

    task

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,50 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+
+const galleryEl = document.querySelector(".gallery");
+
+function createMarkup(arr) {
+  const markup = arr
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+    })
+    .join("");
+  return markup;
+}
+
+const imagesMarkup = createMarkup(galleryItems);
+galleryEl.insertAdjacentHTML("beforeend", imagesMarkup);
+
+galleryEl.addEventListener("click", onClick);
+
+function onClick(event) {
+  event.preventDefault();
+  const containGallery = event.target.classList.contains("gallery__image");
+  if (!containGallery) {
+    return;
+  }
+  const imgUrl = event.target.dataset.source;
+  const instance = basicLightbox.create(
+    `<img src="${imgUrl}" width="800" height="600">`
+  );
+  instance.show();
+
+  window.addEventListener("keydown", onEscPress);
+  function onEscPress(event) {
+    if (event.code === "Escape") {
+      instance.close();
+      window.removeEventListener("keydown", onEscPress);
+    }
+  }
+}
 
 console.log(galleryItems);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,30 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+
+const galleryEl = document.querySelector(".gallery");
+
+function createMarkup(images) {
+  const markup = images
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
+    })
+    .join("");
+  return markup;
+}
+
+const imagesMarkup = createMarkup(galleryItems);
+galleryEl.insertAdjacentHTML("beforeend", imagesMarkup);
+
+// galleryEl.addEventListener("click", onClick);
+
+let gallery = new SimpleLightbox(".gallery a");
+gallery.on("show.simplelightbox", function () {
+  gallery.options.captionsData = "alt";
+  gallery.options.captionsDelay = 250;
+  gallery.options.scrollZoom = false;
+  gallery.options.scrollZoomFactor = 0;
+});
 
 console.log(galleryItems);
